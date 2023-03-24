e1897b72442424f4c31687ea4539beaa8f4bff36
Deepesh.cholker <deepesh.cholkar@codiant.com>
2023-03-24 14:16:46 +0530

    added multer in signup api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const Router = require("express").Router();
 const user = require("../controllers/userControllers");
+const upload = require("../middlewares/multiStoreMiddleware");
 
 Router.post(
     "/signup",
+    upload.single("profilePic"),
     user.signUp
 );
 
